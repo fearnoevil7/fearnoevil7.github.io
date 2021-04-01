c603133b038cdcfa9376d1ee59615ca4055de1dc
fearnoevil7 <56465235+fearnoevil7@users.noreply.github.com>
2021-04-01 17:31:07 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    // navbar-toggler
+    $('.navbar-toggler').click(function(){
+        $('.navbar-toggler').toggleClass('change');
+    })
+
+    // adding background and fixed
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+        if(position >= 149){
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        }
+        else {
+            $('.navbar').removeClass('navbar-background');
+            $('.navbar').removeClass('fixed-top');
+        }
+    })
+
+    // smooth scroll
+    $('.nav-item a').click(function(link){
+        link.preventDefault();
+        
+        // target is a variable
+        let target = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop:$(target).offset().top
+        }, 3000);
+    })
+
+    // init Isotope
+    let $grid = $('.grid').isotope({
+        // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on( 'click', 'button', function() {
+        let filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+
+
+});
